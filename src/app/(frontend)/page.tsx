7d9553ecfa485149ca22740150fcf502e97518f6
Kavindu Sanjula <123143684+RavanaDevs@users.noreply.github.com>
2025-02-14 10:47:55 +0530

    feat: Add tailwindcss 4

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,11 +1,11 @@
 import { headers as getHeaders } from 'next/headers.js'
 import Image from 'next/image'
-import { getPayload } from 'payload'
 import React from 'react'
 import { fileURLToPath } from 'url'
 
 import config from '@/payload.config'
 import './styles.css'
+import { getPayload } from 'payload'
 
 export default async function HomePage() {
   const headers = await getHeaders()
@@ -17,6 +17,9 @@ export default async function HomePage() {
 
   return (
     <div className="home">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <div className="content">
         <picture>
           <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
